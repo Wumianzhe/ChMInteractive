630ea1f5fe9207579c901bcd81d70593dcc2574c
kchnms <pashafizmat1@gmail.com>
2022-10-29 15:20:00 +0300

    fixed basic navigation

--- SimpleInteractions/urls.py
+++ SimpleInteractions/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
-    ,path('index', views.index, name='index')
-    ,path('add', views.add, name='add')
-    ,path('num', views.num, name='numhead')
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('add', views.add, name='add'),
+    path('home', views.home, name='home'),
+    path('about', views.about, name='ABOUT'),
+    path('graph', views.graph, name='GRAPH'),
+    path('theory', views.theory, name='THEORY')
 ]
