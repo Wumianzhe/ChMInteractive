f6f1c4b05bc56a1c5814f0b8ac2b3d6f4abd4cb0
Zero <kulyanin.aa@edu.spbstu.ru>
2022-12-03 02:43:44 +0300

    Changed GRAPH.html
    GRAPH.html renamed to BISECTION.html and add new page (NEWTON.html and SECANT.html)
    Also add output root

--- front/js/main_graph.js
+++ front/js/main_graph.js
@@ -6,6 +6,7 @@ const func_id = 'func'
 const from_id = 'from'
 const to_id = 'to'
 const sub_btn_id = 'sub_btn'
+const res_text_id = 'text_id'
 
 // Получаем элемент DOM-дерева по id и присваиваем значение аттрибуту oninput
 // oninput вызывается с параметром "event" каждый раз, когда ввод меняется
@@ -15,6 +16,7 @@ const from = document.getElementById(from_id)
 
 const to = document.getElementById(to_id)
 
+const res_text = document.getElementById(res_text_id)
 
 const sub_btn = document.getElementById(sub_btn_id);
 
@@ -25,5 +27,8 @@ sub_btn.onclick = async (e) => {
   const obj = { f: func.value, from: from.value, to: to.value };
   const response = await submitAct(obj)
   const data = await response.json()
+  res_text.innerHTML = 'Root: ' + data.result
+  res_text.style.visibility = 'visible'
   setMethod(data, "bisect")
+ 
 }
--- front/js/utils_graph.js
+++ front/js/utils_graph.js
@@ -15,7 +15,7 @@ export const submitAct = async (obj) => {
         origin: 'CHmI',
         headers: { 'Content-Type': 'application/json' },
     };
-    const url = 'http://localhost:8080/bisection_response/?' + args;
+    const url = 'http://192.168.0.106:8080/bisection_response/?' + args;
     console.log(url);
     const t = fetch(url, requestOptions)
     return t
